43dbb6c493ff95cb8eadb7906ed35209cd04af47
Divjot Singh <bogas04@gmail.com>
2018-04-17 00:33:26 +0530

    Add better support

--- src/translit_modules/__tests__/hindi.test.js
+++ src/translit_modules/__tests__/hindi.test.js
@@ -1,8 +1,16 @@
 /* globals describe, it, expect */
-const hindi = require("../hindi");
 
-describe("hindi()", () => {
-  it('handles "soriT mhlw 5 ]"', () => {
-    expect(hindi("soriT mhlw 5 ]")).toBe("सोरठि महला ५ ||");
-  });
+/* Use https://www.lexilogos.com/keyboard/devanagari.htm to add Hindi Unicode */
+const lib = require('../hindi');
+
+const tests = [
+  ['soriT mhlw 5 ]', 'सोरठि महला ५ ||'],
+  ['gur Apuny bilhwrI ]', 'गुर अपुने बलिहारी ||'],
+  ['ijin pUrn pYj svwrI ]', 'जिनि पूरन पैज सवारी ||'],
+];
+
+describe('hindi()', () => {
+  tests.forEach(([input, output]) =>
+    it(`handles "${input}"`, () => expect(lib(input)).toBe(output))
+  );
 });
