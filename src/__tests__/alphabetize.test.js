88671c68d53c16db5d8190d9bb6275d2a4ebf6d9
gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2019-02-12 11:08:36 +0530

    Improves alphabetize tests to check all letters while sorting

--- src/__tests__/alphabetize.test.js
+++ src/__tests__/alphabetize.test.js
@@ -3,17 +3,21 @@ const alphabetize = require('../alphabetize');
 describe('Sorting a list of shabads', () => {
   it('Should return sorted shabad list (unicode)', () => {
     const shabads = [
-      'ਕੇਤੀਆ ਸੁਰਤੀ ਸੇਵਕ ਕੇਤੇ ਨਾਨਕ ਅੰਤੁ ਨ ਅੰਤੁ ॥੩੫॥',
+      'ਕਥਨਾ ਕਥੀ ਨ ਆਵੈ ਤੋਟਿ ॥',
       'ਤੁਧੁ ਵਿਣੁ ਸਿਧੀ ਕਿਨੈ ਨ ਪਾਈਆ ॥',
       'ਬਿਨੁ ਸਤਿਗੁਰ ਕਿਨੈ ਨ ਪਾਇਓ ਕਰਿ ਵੇਖਹੁ ਮਨਿ ਵੀਚਾਰਿ ॥',
       'ਅੰਤੁ ਨ ਸਿਫਤੀ ਕਹਣਿ ਨ ਅੰਤੁ ॥',
       'ਹਉ ਹਉ ਕਰਤੀ ਸਭ ਮੁਈ ਸੰਪਉ ਕਿਸੈ ਨ ਨਾਲਿ ॥',
+      'ਕਥਿ ਕਥਿ ਕਥੀ ਕੋਟੀ ਕੋਟਿ ਕੋਟਿ ॥',
+      'ਕੇਤੀਆ ਸੁਰਤੀ ਸੇਵਕ ਕੇਤੇ ਨਾਨਕ ਅੰਤੁ ਨ ਅੰਤੁ ॥੩੫॥',
     ];
     expect(alphabetize(shabads, 'unicode'))
       .toEqual([
         'ਅੰਤੁ ਨ ਸਿਫਤੀ ਕਹਣਿ ਨ ਅੰਤੁ ॥',
         'ਹਉ ਹਉ ਕਰਤੀ ਸਭ ਮੁਈ ਸੰਪਉ ਕਿਸੈ ਨ ਨਾਲਿ ॥',
         'ਕੇਤੀਆ ਸੁਰਤੀ ਸੇਵਕ ਕੇਤੇ ਨਾਨਕ ਅੰਤੁ ਨ ਅੰਤੁ ॥੩੫॥',
+        'ਕਥਿ ਕਥਿ ਕਥੀ ਕੋਟੀ ਕੋਟਿ ਕੋਟਿ ॥',
+        'ਕਥਨਾ ਕਥੀ ਨ ਆਵੈ ਤੋਟਿ ॥',
         'ਤੁਧੁ ਵਿਣੁ ਸਿਧੀ ਕਿਨੈ ਨ ਪਾਈਆ ॥',
         'ਬਿਨੁ ਸਤਿਗੁਰ ਕਿਨੈ ਨ ਪਾਇਓ ਕਰਿ ਵੇਖਹੁ ਮਨਿ ਵੀਚਾਰਿ ॥',
       ]);
@@ -21,17 +25,21 @@ describe('Sorting a list of shabads', () => {
 
   it('Should return sorted shabad list (english)', () => {
     const shabads = [
-      'kyqIAw surqI syvk kyqy nwnk AMqu n AMqu ]35]',
+      'kQnw kQI n AwvY qoit ]',
       'quDu ivxu isDI iknY n pweIAw ]',
       'ibnu siqgur iknY n pwieE kir vyKhu min vIcwir ]',
       'AMqu n isPqI khix n AMqu ]',
       'hau hau krqI sB mueI sMpau iksY n nwil ]',
+      'kiQ kiQ kQI kotI koit koit ]',
+      'kyqIAw surqI syvk kyqy nwnk AMqu n AMqu ]35]',
     ];
     expect(alphabetize(shabads))
       .toEqual([
         'AMqu n isPqI khix n AMqu ]',
         'hau hau krqI sB mueI sMpau iksY n nwil ]',
         'kyqIAw surqI syvk kyqy nwnk AMqu n AMqu ]35]',
+        'kiQ kiQ kQI kotI koit koit ]',
+        'kQnw kQI n AwvY qoit ]',
         'quDu ivxu isDI iknY n pweIAw ]',
         'ibnu siqgur iknY n pwieE kir vyKhu min vIcwir ]',
       ]);
