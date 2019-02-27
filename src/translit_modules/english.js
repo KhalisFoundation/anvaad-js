57db245295e0a3950a77933b30dd9d9240ab6355
Tarun Singh <35271830+tarunsingh5@users.noreply.github.com>
2019-02-27 11:09:26 -0800

    Update english.js

--- src/translit_modules/english.js
+++ src/translit_modules/english.js
@@ -269,7 +269,7 @@ module.exports = (gurmukhi = '') => {
 
     // 4 Transliterate as "ri" if
     if (
-      thisLetter === "i" //step2Values[step2Keys.indexOf('i')] && // current letter is sihari
+      thisLetter === 'i' //step2Values[step2Keys.indexOf('i')] && // current letter is sihari
       trans[x + 2] === step2Values[step2Keys.indexOf('R')]  // letter afeter next is pehar rara
       // nextLetter === step2Values[step2Keys.indexOf('R')] // next letter is pehar rara
     ) {
